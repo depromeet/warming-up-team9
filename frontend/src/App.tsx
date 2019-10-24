d9959c2c71254f1a1b20e3aaad1c445912a7f0a3
leejiwonn <37164467+leejiwonn@users.noreply.github.com>
2019-10-24 14:25:25 +0900

    signup route 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { Router, Switch, Route } from 'react-router-dom';
 import Login from './containers/Login';
 import { createBrowserHistory } from 'history';
 import Main from './containers/Main';
+import SignUp from './containers/SignUp';
 
 const history = createBrowserHistory({ basename: '/' });
 
@@ -17,7 +18,11 @@ export default function App() {
         <Route exact={true} path="/">
           <Main />
         </Route>
+        <Route exact={true} path="/signup">
+          <SignUp />
+        </Route>
       </Switch>
     </Router>
   );
 }
+
