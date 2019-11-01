f90556c5703bd23756fb1ad69e561ea35b382082
JiwoonKim <diana0501@naver.com>
2019-11-02 01:19:08 +0900

    CreateTask UI 그리기 완료
    * Task 추가 컨테이너인 CreateTask의 UI 구성 완료

--- frontend/src/containers/CreateTask/index.jsx
+++ frontend/src/containers/CreateTask/index.jsx
@@ -1,20 +1,67 @@
 import React from 'react';
-/** @jsx jsx */
-import {css, jsx} from '@emotion/core';
-import Button from '../../components/Button'
-import Dropdown from '../../components/Dropdown'
+import Button from '../../components/Button';
+import './styles.css';
 
 export default class CreateTask extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+    // TODO: 스토어로부터 user.nickname 받기
 
-    return (<div>
-        <h2>환영합니다, 님!</h2>
-        <h2>지금 해야 할 일을 추가해볼까요?</h2>
-        <Dropdown />
-        <div>
-            <Button>건너뛸래요</Button>
-            <Button>작성완료</Button>
+    // TODO: 백엔드로부터 Task List 받기
+    this.state = {
+      listItems: [
+        '디프만 자소서 완성하기',
+        '디프만 신청서 작성하기',
+        '워밍업 프로젝트 기획안 작성',
+        '워밍업 프로젝트 와이어프레임',
+        '워밍업 프로젝트 GUI 디자인',
+        '로고 디자인',
+        '디프만 보고서 쓰기',
+      ],
+    };
+  }
+
+  render() {
+    const tasks = this.state.listItems;
+
+    return (
+      <div className="createTask-container">
+        <div className="header">
+          <span className="header-text">환영합니다, 님!</span>
+          <strong className="header-text">지금 해야 할 일을 추가해볼까요?</strong>
         </div>
-    </div>);
+        <div className="add-task">
+            <input
+              type={this.props.type}
+              name={this.props.name}
+              placeholder={this.props.placeholder}
+              list="options"
+              className="new-task-input"
+            />
+            <Button onClick={this.props.handleClick} className="add-task-btn">
+              추가하기
+            </Button>
+        </div>
+        <ul className="dropdown-list">
+            {tasks.map(task => (
+                <li className="dropdown-list-item">
+                    <span className="item-name">{task}</span>
+                    <Button className="delete-task-btn"></Button>
+                </li>
+            ))}
+        </ul>
+        <div className="next-btns">
+          <Button className="skip-btn">건너뛸래요</Button>
+          <Button className="done-btn">작성완료</Button>
+        </div>
+      </div>
+    );
   }
 }
+
+CreateTask.defaultProps = {
+  type: 'text',
+  name: 'input',
+  placeholder: '해야할 Task를 적어주세요',
+  handleClick: console.log('no handleClick prop passed'),
+};
