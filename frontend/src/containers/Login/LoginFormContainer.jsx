054d3a92e7cead9ba39cef5d9f5e1ee0afc74019
leejiwonn <37164467+leejiwonn@users.noreply.github.com>
2019-11-02 16:13:43 +0900

    로그인 페이지 코드 수정했습니다

--- frontend/src/containers/Login/LoginFormContainer.jsx
+++ frontend/src/containers/Login/LoginFormContainer.jsx
@@ -1,37 +1,10 @@
 import React, { useState } from 'react';
 import { withRouter } from 'react-router-dom';
 import LoginForm from '../../components/LoginForm'
-// import validate from './LoginFormValidationRules';
-// import useRequest from '../../lib/hooks/useRequest';
-// import { loginRequest } from '../../lib/api/auth';
 
-const LoginFormContainer = ({history}) => {
-  const [errors, setErrors] = useState(null);
-  const [onLogin] = useRequest(loginRequest);
-
-  const onSubmit = async (form) => {
-    setErrors(null);
-    const validation = validate(form); 
-    if(validation){
-      setErrors(validation);
-      return;
-    }
-
-    try {
-      await onLogin(form);
-      // history.push('/');
-    } catch (e) {
-      if(e.response.status === 409){
-        setErrors({common : '이미 존재하는 이메일 입니다. !'});
-      }
-      setErrors({common : '에러 발생 !'});
-    }
-  }
+const LoginFormContainer = ({}) => {
   return (
-    <LoginForm
-      onSubmit={onSubmit}
-      errors={errors}
-    />
+    <LoginForm />
   )
 }
-export default withRouter(LoginFormContainer);
+export default LoginFormContainer;
