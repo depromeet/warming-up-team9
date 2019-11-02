017b6f6d9144a229543a6bc48020ddf72291c471
seokju-na <seokju.me@gmail.com>
2019-11-02 18:47:43 +0900

    feat(frontend): 메인 헤더 추가 (#12)
    * 의존성 추가
    
    * <Header /> 컴포넌트 추가
    
    * css 수정
    
    * api 경로 수정

--- frontend/src/remotes/api.js
+++ frontend/src/remotes/api.js
@@ -3,7 +3,7 @@ import * as axios from 'axios';
 export const API_ROOT = '_api';
 
 export async function fetchUser(authToken) {
-  const { data } = await axios.get(`${API_ROOT}/auth`, {
+  const { data } = await axios.get(`${API_ROOT}/users/me`, {
     headers: {
       Authorization: authToken,
     },
