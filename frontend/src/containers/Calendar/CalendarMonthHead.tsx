f91ca15cdb2374c19a3fbe16ed3c22cedcaf69a1
seokju-na <seokju.me@gmail.com>
2019-11-02 17:07:22 +0900

    <CalendarMonth /> 컴포넌트 추가

---
+++ frontend/src/containers/Calendar/CalendarMonthHead.tsx
@@ -0,0 +1,40 @@
+import styled from '@emotion/styled';
+import React from 'react';
+import { HEAD_SIZE } from './sizes';
+
+export default function CalendarMonthHead() {
+  return (
+    <MonthHead>
+      <tr>
+        <td>Sun</td>
+        <td>Mon</td>
+        <td>Tue</td>
+        <td>Wed</td>
+        <td>Thu</td>
+        <td>Fri</td>
+        <td>Sat</td>
+      </tr>
+    </MonthHead>
+  );
+}
+
+const MonthHead = styled.thead`
+  display: block;
+  width: 100%;
+
+  & > tr {
+    display: flex;
+    align-items: flex-start;
+    height: ${HEAD_SIZE}px;
+
+    & > td {
+      flex: 1 1 14.285%; // 100 / 7 = 14.285%
+
+      font-size: 13px;
+      font-weight: bold;
+      letter-spacing: -0.6px;
+      text-align: center;
+      color: #bac5d3;
+    }
+  }
+`;
