280896578034add6021360828786cf45624e321e
seokju-na <seokju.me@gmail.com>
2019-11-02 18:46:40 +0900

    css 수정

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
-import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
-import { Router, Switch, Route, Redirect } from 'react-router-dom';
+import { Redirect, Route, Router, Switch } from 'react-router-dom';
 import Login from './containers/Login';
 import { createBrowserHistory } from 'history';
 import Main from './containers/Main';
@@ -10,6 +9,9 @@ import { fetchUser } from './remotes/api';
 import { withStore } from './stores';
 import { selectAuthToken, selectIsCheckingAuth, selectIsCommonInitialized, selectUser } from './stores/selectors';
 import { checkAuthRequestAction, checkAuthResponseAction } from './stores/actions';
+import Header from './containers/Header';
+import styled from '@emotion/styled';
+import { HEADER_SIZE } from './containers/Header/sizes';
 
 const history = createBrowserHistory({ basename: '/' });
 
@@ -54,20 +56,36 @@ function App() {
 
   return (
     <Router history={history}>
-      <Switch>
-        <Route exact={true} path="/login">
-          <Login />
-        </Route>
-        <Route exact={true} path="/">
-          {user != null ? <Main /> : <Redirect to="/login" />}
-        </Route>
-        <Route exact={true} path="/signup">
-          <SignUp />
-        </Route>
-      </Switch>
+      <Wrapper>
+        <Header user={user} />
+        <Content>
+          <Switch>
+            <Route exact={true} path="/login">
+              <Login />
+            </Route>
+            <Route exact={true} path="/">
+              {user != null ? <Main /> : <Redirect to="/login" />}
+            </Route>
+            <Route exact={true} path="/signup">
+              <SignUp />
+            </Route>
+          </Switch>
+        </Content>
+      </Wrapper>
     </Router>
   );
 }
 
 export default withStore(App);
 
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const Content = styled.main`
+  position: relative;
+  display: block;
+  width: 100%;
+  height: calc(100% - ${HEADER_SIZE}px);
+`;
