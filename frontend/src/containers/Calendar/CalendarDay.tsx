d1c7aba03620ef6e61add4bf17514640c5770480
seokju-na <seokju.me@gmail.com>
2019-11-03 16:59:58 +0900

    feat(frontend): 캘린더에 dot, 당근 이미지 추가 (#19)
    * README 추가
    
    * feat(frontend): dot, carrot 이미지 추가

--- frontend/src/containers/Calendar/CalendarDay.tsx
+++ frontend/src/containers/Calendar/CalendarDay.tsx
@@ -2,11 +2,12 @@ import styled from '@emotion/styled';
 import { format } from 'date-fns';
 import React, { memo, MouseEvent, useCallback, useMemo } from 'react';
 import { cloneDate } from '../../utils';
-import { DAY_CELL_SIZE } from './sizes';
+import carrotImage from './carrot.svg';
 
 interface Props {
   day: Date;
   showCarrot?: boolean;
+  showDot?: boolean;
   isEmptyCell?: boolean;
   isSelected?: boolean;
   isDisabled?: boolean;
@@ -22,7 +23,16 @@ function isLastDayOfWeek(date: Date) {
   return date.getDay() === 6;
 }
 
-function CalendarDay({ day, isEmptyCell, isSelected, isDisabled, tabIndex = -1, onDayClick }: Props) {
+function CalendarDay({
+  day,
+  showDot,
+  showCarrot,
+  isEmptyCell,
+  isSelected,
+  isDisabled,
+  tabIndex = -1,
+  onDayClick,
+}: Props) {
   const content = useMemo(() => format(day, 'd'), [day]);
   const handleClick = useCallback(
     (event: MouseEvent) => {
@@ -50,6 +60,8 @@ function CalendarDay({ day, isEmptyCell, isSelected, isDisabled, tabIndex = -1, 
       lastDayOfWeek={isLastDayOfWeek(day)}
     >
       <Inner>{isEmptyCell ? '' : content}</Inner>
+      {showDot && !isEmptyCell && <Dot />}
+      {showCarrot && !isEmptyCell && <CarrotImage src={carrotImage} aria-hidden={true} alt="" />}
     </Day>
   );
 }
@@ -71,19 +83,43 @@ const Day = styled.td<{
   flex-direction: column;
   align-items: center;
 
-  height: ${DAY_CELL_SIZE}px;
-
   border: none;
   margin: 0;
-  padding: 0;
+  padding: 2px;
 
   user-select: none;
 
   cursor: ${props => (props.disabled || props.empty ? 'default' : 'cursor')};
+
+  ${props =>
+    props.selected
+      ? `
+    border-radius: 8px;
+    background-color: #44484e;
+  `
+      : ''};
 `;
 
 const Inner = styled.span`
   font-size: 12px;
   font-weight: bold;
   color: #e8e6e6;
+  line-height: 18px;
+`;
+
+const Dot = styled.span`
+  width: 6px;
+  height: 6px;
+  background-color: #ff5001;
+  border-radius: 50%;
+  margin-top: 4px;
+`;
+
+const CarrotImage = styled.img`
+  width: 17px;
+  height: 17px;
+  position: absolute;
+  display: inline-block;
+  right: 4px;
+  top: 3px;
 `;
