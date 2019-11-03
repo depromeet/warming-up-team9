8c254418ce36a62ff6002345ba104608294ddf9c
SangHyoun Yun <hainco@gmail.com>
2019-11-04 00:11:59 +0900

    Mongoose 추가 및 connection 설정

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,7 @@ import express from "express";
 import logger from "morgan";
 import * as middlewares from "./middlewares";
 import router from "./router";
+import * as db from "./models";
 
 const PORT = 3000;
 
@@ -16,6 +17,15 @@ app.use(router(express.Router()));
 app.use(middlewares.notFoundCreator);
 app.use(middlewares.errorHandler);
 
-app.listen(PORT, () => {
-    console.log(`Server is Listening on port ${PORT}`);
-});
+(async () => {
+    try {
+        await db.init();
+
+        app.listen(PORT, () => {
+            console.log(`Server is Listening on port ${PORT}`);
+        });
+    } catch (err) {
+        console.log(err);
+        process.exit(1);
+    }
+})();
---
+++ backend/src/models/index.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import config from "../config";
+
+const env: string = process.env.NODE_ENV || "development";
+
+export const init = async () => {
+    try {
+        mongoose.set("useUnifiedTopology", true);
+        const connection = await mongoose.connect(config.mongo[env], { useNewUrlParser: true });
+        console.log(`MongoDB Connection Success [ENV: ${env}]`);
+        return connection;
+    } catch (e) {
+        throw `MongoDB Connection Fail, ${e.message}`;
+    }
+};
