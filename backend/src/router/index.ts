f13d15a385a527d86d82cf92601d986c38d94d5f
SangHyoun Yun <hainco@gmail.com>
2019-11-04 00:43:03 +0900

    signUp API 추가

--- backend/src/router/index.ts
+++ backend/src/router/index.ts
@@ -3,6 +3,7 @@ import * as users from "./users";
 
 const router = (router: express.Router) => {
     router.route("/login").post(users.login);
+    router.route("/signup").post(users.signUp);
 
     return router;
 };
--- backend/src/router/users/auth.ts
+++ backend/src/router/users/auth.ts
@@ -1,4 +1,8 @@
 import { RequestHandler } from "express";
+import Joi from "@hapi/joi";
+import bcryptjs from "bcryptjs";
+import createHttpError from "http-errors";
+import { db } from "../../models";
 
 export const login: RequestHandler = (req, res, next) => {
     try {
@@ -7,5 +11,39 @@ export const login: RequestHandler = (req, res, next) => {
         });
     } catch (err) {
         throw err;
+    }
+};
+
+export const signUp: RequestHandler = async (req, res, next) => {
+    try {
+        const schema = Joi.object({
+            email: Joi.string()
+                .email()
+                .required(),
+            nickname: Joi.string()
+                .min(1)
+                .max(10)
+                .required(),
+            password: Joi.string()
+                .min(6)
+                .max(50)
+                .alphanum()
+                .required(),
+        });
+        const { email, nickname, password } = await schema.validateAsync(req.body).catch(err => {
+            throw createHttpError(400, err.message);
+        });
+
+        const passwordHash = await bcryptjs.hashSync(password);
+        const user = new db.Users({
+            email,
+            nickname,
+            passwordHash,
+        });
+        await user.save();
+
+        res.send(user);
+    } catch (err) {
+        next(err);
     }
 };
