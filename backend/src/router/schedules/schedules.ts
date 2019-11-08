f1c7eff9c8fb2a9dbe3a04027becb220d0e72b41
SangHyoun Yun <hainco@gmail.com>
2019-11-09 01:39:27 +0900

    schedule 추가 api 에서 string 사용

--- backend/src/router/schedules/schedules.ts
+++ backend/src/router/schedules/schedules.ts
@@ -1,18 +1,23 @@
 import { RequestHandler } from "express";
 import Joi from "@hapi/joi";
+import moment from "moment";
 import { scheduleService } from "../../services";
 
 export const addSchedule: RequestHandler = async (req, res, next) => {
     try {
         const { scheduleDate, taskId, estimatedHour } = await Joi.object({
-            scheduleDate: Joi.date().required(),
+            scheduleDate: Joi.string().required(),
             taskId: Joi.string().required(),
             estimatedHour: Joi.number()
                 .min(1)
                 .max(12),
         }).validateAsync(req.body);
 
-        await scheduleService.addSchedule({ owner: req.user.uid, taskId, estimatedHour, scheduleDate });
+        const date = moment(scheduleDate)
+            .startOf("day")
+            .toDate();
+
+        await scheduleService.addSchedule({ owner: req.user.uid, taskId, estimatedHour, scheduleDate: date });
         res.sendStatus(201);
     } catch (err) {
         next(err);
